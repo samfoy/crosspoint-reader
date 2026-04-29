bc167cbf309dfe7d4cd22a6046e3e65762458ed1
jpirnay <jens@pirnay.com>
2026-04-29 16:31:00 +0200

    Add fonts from sd-fonts repo and add font size 10 to reader overrides

--- src/activities/settings/FontDownloadActivity.h
+++ src/activities/settings/FontDownloadActivity.h
@@ -8,7 +8,7 @@
 #include "util/ButtonNavigator.h"
 
 #ifndef FONT_MANIFEST_URL
-#define FONT_MANIFEST_URL "https://github.com/jpirnay/crosspoint-reader/releases/download/sd-fonts/fonts.json"
+#define FONT_MANIFEST_URL "https://github.com/jpirnay/crosspoint-reader/assets/sd-fonts/fonts.json"
 #endif
 
 class FontDownloadActivity : public Activity {
